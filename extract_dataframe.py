37f785351b7e93abad4ccaccc2cf03f4e9aa9f9e
zelalem shiferaw <zelalemshiferaw65@gmail.com>
2022-08-09 00:59:47 +0300

    fixed functions find_followers_count,find_friends_count

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -66,15 +66,19 @@ class TweetDfExtractor:
 
     def find_screen_name(self)->list:
         screen_name = [tweet['user']['screen_name']
-                       for tweet in self.tweets_list]
+                    for tweet in self.tweets_list]
         return screen_name
 
 
     def find_followers_count(self)->list:
-        followers_count = 
+        followers_count = [tweet['user']['followers_count']
+                        for tweet in self.tweets_list]
+        return followers_count
 
     def find_friends_count(self)->list:
-        friends_count = 
+        friends_count = [tweet['user']['friends_count']
+                         for tweet in self.tweets_list]
+        return friends_count
 
     def is_sensitive(self)->list:
         try:
