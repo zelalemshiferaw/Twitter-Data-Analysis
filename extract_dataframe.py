969e861a2b549a4aa541b044d9d922f11ae95cae
zelalem shiferaw <zelalemshiferaw65@gmail.com>
2022-08-09 01:05:08 +0300

    get global_twitter_data.json

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json("/data/global_twitter_data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
