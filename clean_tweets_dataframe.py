d95cbf69256b0edca8c921be511dc5650a58d1f7
zelalem shiferaw <zelalemshiferaw65@gmail.com>
2022-08-09 04:41:48 +0300

    fixed functions drop duplicates,convert_to_datetime and convert_to_numbers

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import re
+from datetime import datetime
+
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -20,18 +24,16 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
-        
-        ---
+        df = df.drop_duplicates()
+        return df
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
-        
+        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
+
         df = df[df['created_at'] >= '2020-12-31' ]
         
         return df
@@ -41,11 +43,14 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
-        
+        df['polarity'] = pd.to_numeric(df['polarity'], errors='coerce')
+        df['subjectivity'] = pd.to_numeric(df['subjectivity'], errors='coerce')
+        df['retweet_count'] = pd.to_numeric(
+            df['retweet_count'], errors='coerce')
+        df['favorite_count'] = pd.to_numeric(
+            df['favorite_count'], errors='coerce')
+        df['followers_count'] = pd.to_numeric(
+            df['followers_count'], errors='coerce')
         return df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
