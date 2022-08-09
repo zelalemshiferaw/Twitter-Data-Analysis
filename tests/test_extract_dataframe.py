615f36690ba7c55d73964b51941b564a07b9146d
zelalem shiferaw <zelalemshiferaw65@gmail.com>
2022-08-09 10:02:13 +0300

    update sample json path

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -11,7 +11,7 @@ from extract_dataframe import TweetDfExtractor
 # we will need about 5 tweet samples. 
 # Create a sample not more than 10 tweets and place it in a json file.
 # Provide the path to the samples tweets file you created below
-sampletweetsjsonfile = "global_twitter_test_data.json"   #put here the path to where you placed the file e.g. ./sampletweets.json. 
+sampletweetsjsonfile = "./tests/global_twitter_test_data.json"   #put here the path to where you placed the file e.g. ./sampletweets.json. 
 _, tweet_list = read_json(sampletweetsjsonfile)
 
 columns = [
